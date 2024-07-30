a8b286a5a575cb1b09f9bb296a4eb884b07e6d37
JacobPaff <20961612@students.latrobe.edu.au>
2024-07-30 08:29:04 +0000

    Init Commit

--- cse3cbx_webapp/cse3cbx_webapp/urls.py
+++ cse3cbx_webapp/cse3cbx_webapp/urls.py
@@ -1,4 +1,4 @@
-"""hello_world URL Configuration
+"""cse3cbx_webapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,17 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-from hello_world.core import views as core_views
+from django.urls import path
 
 urlpatterns = [
-    path("", core_views.index),
     path("admin/", admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
